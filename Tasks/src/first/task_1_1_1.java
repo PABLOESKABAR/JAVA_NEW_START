9a355730221eaccb5c2fc6d8d1e204ad42ffda2c
pavchichirko <8750202@mail.ru>
2021-10-31 21:39:30 +0300

    first commit 123

--- Tasks/src/first/task_1_1_1.java
+++ Tasks/src/first/task_1_1_1.java
@@ -1,8 +1,19 @@
 package first;
 
+import java.util.Scanner;
+
 public class task_1_1_1 {
     public static void main(String[] args) {
-        double a, b, c;
-        
+
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите значение а");
+        double a = console.nextInt();
+        System.out.println("Введите значение b");
+        double b = console.nextInt();
+        System.out.println("Введите значение c");
+        double c = console.nextInt();
+        double z = (((a-3)*b)/2)+c;
+        System.out.println("Значение z = " + z);
+
     }
 }
